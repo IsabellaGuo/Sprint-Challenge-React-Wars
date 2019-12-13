93d6831004b24478de33f384e146ac68b4b3c86c
IsabellaGuo <isabella.guofei@gmail.com>
2019-12-13 09:21:07 -0800

    PeopleCard styling is done

--- starwars/src/components/People.js
+++ starwars/src/components/People.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const PeopleDiv = styled.div`
+    background-color: #ffa259;
+    width: 25%;
+    margin: 2rem;
+    border: 5px solid #fa4252;
+  `;
 const PeopleCard = (props) => {
    console.log(props);
 
     return (
         
-        <div>
+        <PeopleDiv>
           <h2> {props.name}</h2>
           <h3>Born:{props.birthyear}</h3>
           <h3>Height: {props.height}</h3>
-        </div>
+        </PeopleDiv>
     )
 }
 
